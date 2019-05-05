19668945f9648dd4b0d7486a1ba2d3697586e02e
ricardo <ricardo.de.oliveira96@gmail.com>
2019-05-05 16:54:06 -0300

    "Alguns teste dp framework de css e php"

--- web/projeto/mvc/Visao/mensagens/index.php
+++ web/projeto/mvc/Visao/mensagens/index.php
@@ -1,23 +1,43 @@
-<div class="center-block site">
-    <h1 class="text-center">Chat Online</h1>
-    <h2>Escreva a mensagem</h2>
-    <div class="clearfix margin-bottom">
-        <form action="<?= URL_RAIZ ?>" method="post" class="form-inline pull-left">
-            <div class="form-group">
-                <input id="usuario" name="usuario" class="form-control campo-form" autofocus placeholder="Usuário">
+
+<div class="col s6 m6">
+    <div class="card  darken-1">
+        <div class="card-content ">
+
+            <div class="center-block site">
+                <h1 class="text-center">Chat Online</h1>
+                <h2>Escreva a mensagem</h2>
+                <div class="clearfix margin-bottom">
+                    <form action="<?= URL_RAIZ ?>" method="post" class="form-inline pull-left">
+                        <div class="form-group">
+                            <input id="usuario" name="usuario" class="form-control campo-form" autofocus placeholder="Usuário">
+                        </div>
+                        <div class="form-group">
+                            <input id="texto" name="texto" class="form-control campo-form" placeholder="Texto">
+                        </div>
+                        <button type="submit" class="btn btn-default">Enviar mensagem</button>
+                    </form>
+                </div>
+                zgsadhdaf
+                <h2>Mensagens</h2>
+                <div class="row">
+                    <?php foreach ($mensagens as $mensagem) : ?>
+
+                        <div class="col s6 m6">
+                            <div class="card grey darken-1">
+                                <div class="card-content white-text">
+                                    <span class="card-title"><strong><?= $mensagem->getUsuario() ?>:</strong></span>
+                                    <p><?= $mensagem->getTexto() ?></p>
+                                </div>
+
+                            </div>
+                        </div>
+
+
+
+
+
+
+                    <?php endforeach ?>
+                </div>
             </div>
-            <div class="form-group">
-                <input id="texto" name="texto" class="form-control campo-form" placeholder="Texto">
-            </div>
-            <button type="submit" class="btn btn-default">Enviar mensagem</button>
-        </form>
-    </div>
-zgsadhdaf
-    <h2>Mensagens</h2>
-    <?php foreach ($mensagens as $mensagem) : ?>
-        <p>
-            <strong><?= $mensagem->getUsuario() ?>:</strong>
-            <?= $mensagem->getTexto() ?>
-        </p>
-    <?php endforeach ?>
-</div>
+        </div></div></div>
--- web/projeto/mvc/Visao/templates/index.php
+++ web/projeto/mvc/Visao/templates/index.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="stylesheet" href="<?= URL_CSS . 'geral.css' ?>">
+    <link rel="stylesheet" href="<?= URL_CSS . 'footer.css' ?>">
     <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Import materialize.css-->
@@ -17,14 +18,59 @@
 
 
 </head>
-<body bgcolor="red">
+<body >
+<header>
 
-<div class="container">
+    <nav> <div class="container">
+        <div class="nav-wrapper">
 
-    <?php $this->imprimirConteudo() ?>
+            <a href="#" class="brand-logo ">Logo</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="sass.html">Sass</a></li>
+                <li><a href="badges.html">Components</a></li>
+                <li><a href="collapsible.html">JavaScript</a></li>
+            </ul>
+            </div>
+        </div>
+    </nav>
+</header>
+<main>
+
+    <div class="container">
+
+        <?php $this->imprimirConteudo() ?>
 
 
-</div>
+    </div>
+</main>
+
+
+<footer class="site-footer page-footer">
+
+        <div class="container">
+            <div class="row">
+                <div class="col l6 s12">
+                    <h5 class="white-text">Footer Content</h5>
+                    <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                </div>
+                <div class="col l4 offset-l2 s12">
+                    <h5 class="white-text">Links</h5>
+                    <ul>
+                        <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <div class="container">
+                © 2014 Copyright Text
+                <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+        </div>
+    </footer>
 <script type="text/javascript" src="<?= URL_JS . 'materialize.min.js' ?>"></script>
 </body>
 </html>
